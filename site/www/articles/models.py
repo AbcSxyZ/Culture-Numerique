0f77275b8155763c685b1fedebf1be981414007f
AbcSxyZ <rossi.sim@outlook.com>
2020-11-06 18:45:52 +0100

    Send mail on new comment

--- site/www/articles/models.py
+++ site/www/articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.mail import mail_admins
 
 # Create your models here.
 
@@ -8,4 +9,10 @@ class Commentary(models.Model):
     comment = models.TextField(verbose_name="Commentaire")
     article = models.TextField(null=False)
     date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        msg = "Nouveau commentaire de {}:\n\n{}\n"
+        msg = msg.format(self.username, self.comment)
+        super().save(*args, **kwargs)
+        mail_admins(subject="Nouveau commentaire", message=msg)
 
